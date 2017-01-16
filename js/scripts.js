b25c3d9cd55c85192272f448e7e2022257a39961
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-16 15:41:19 -0800

    create js for - * /

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,28 @@ $(document).ready(function() {
     var result = add(number1, number2);
     $("#output").text(result);
   });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number2, number2);
+    $("#output").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    even.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
 });
