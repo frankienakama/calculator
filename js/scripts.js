a528e3d9b980f80dfd3d6eaf9a193641ded35d0c
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-16 16:15:01 -0800

    create forms for subtract and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
-var subract = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
 };
 var multiply = function(number1, number2) {
